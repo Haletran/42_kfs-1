4ecf9c1d964cbbefe2c894874f9e15c7e386d836
Haletran <bpasquier123@gmail.com>
2025-12-02 12:08:31 +0100

    add: working Makefile and clean build process

--- build.zig
+++ build.zig
@@ -30,7 +30,65 @@ pub fn build(b: *std.Build) void {
         "kernel.o",
     );
 
-    b.default_step.dependOn(&install_kernel.step);
+    const mkdir_zig_out = b.addSystemCommand(&.{ "mkdir", "-p", "zig-out" });
+
+    const compile_asm = b.addSystemCommand(&.{
+        "./tools/bin/i686-elf-as",
+        "src/boot.s",
+        "-o",
+        "zig-out/boot.o",
+    });
+    compile_asm.step.dependOn(&mkdir_zig_out.step);
+
+    const link_everything = b.addSystemCommand(&.{
+        "./tools/bin/i686-elf-gcc",
+        "-T",
+        "src/linker.ld",
+        "-o",
+        "zig-out/kfs.bin",
+        "-ffreestanding",
+        "-O2",
+        "-nostdlib",
+        "zig-out/boot.o",
+        "zig-out/kernel.o",
+        "-lgcc",
+    });
+    link_everything.step.dependOn(&install_kernel.step);
+    link_everything.step.dependOn(&compile_asm.step);
+
+    const verify_multiboot = b.addSystemCommand(&.{
+        "grub-file",
+        "--is-x86-multiboot",
+        "zig-out/kfs.bin",
+    });
+    verify_multiboot.step.dependOn(&link_everything.step);
+
+    const mkdir_iso_boot = b.addSystemCommand(&.{ "mkdir", "-p", "iso/boot" });
+    const copy_to_iso = b.addSystemCommand(&.{
+        "cp",
+        "zig-out/kfs.bin",
+        "iso/boot/kfs.bin",
+    });
+    copy_to_iso.step.dependOn(&verify_multiboot.step);
+    copy_to_iso.step.dependOn(&mkdir_iso_boot.step);
+
+    const create_iso = b.addSystemCommand(&.{
+        "grub-mkrescue",
+        "-o",
+        "zig-out/kfs.iso",
+        "iso",
+    });
+    create_iso.step.dependOn(&copy_to_iso.step);
+
+    b.default_step.dependOn(&create_iso.step);
+
+    const run_step = b.step("run", "Run the application");
+    const run_cmd = b.addSystemCommand(&.{
+        "qemu-system-i386",
+        "-cdrom",
+        "zig-out/kfs.iso",
+    });
+    run_step.dependOn(&run_cmd.step);
 
     const clean_step = b.step("clean", "Remove build artifacts");
     const clean_cmd = b.addSystemCommand(&.{
@@ -39,6 +97,7 @@ pub fn build(b: *std.Build) void {
         "zig-out",
         "zig-cache",
         "out",
+        "iso/boot/kfs.bin",
     });
     clean_step.dependOn(&clean_cmd.step);
 }
